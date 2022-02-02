5d86e29f42efece8a2ac992eb1eabd8fd1115cb9
Jason Forsyth <forsy2jb@jmu.edu>
2022-02-01 23:04:58 -0500

    Add function examples

---
+++ lecture-examples/week3-creating-functions.py
@@ -0,0 +1,25 @@
+# make a simple function to test for even or odd
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+# make a helper function to calculate the average of some list
+def average(nums):
+    total = 0
+    for n in nums:
+        total += n
+
+    return total / len(nums)
+
+
+# create a new list of integers
+new_list = [2, 3, 4, 5, 6]
+
+# calculate the average
+avg = average(new_list)
+print("Average is: ",avg)
+
+
