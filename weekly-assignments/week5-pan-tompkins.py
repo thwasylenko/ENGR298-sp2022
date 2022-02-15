91a44231565af4a58e69dd2c4664e5929b257224
Jason Forsyth <forsy2jb@jmu.edu>
2022-02-15 18:33:33 -0500

    Add EKG assignments

---
+++ weekly-assignments/week5-pan-tompkins.py
@@ -0,0 +1,35 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import the CSV file using numpy
+path = '../data/ekg/mitdb_201.csv'
+
+# load data in matrix from CSV file; skip first two rows
+## your code here
+
+# save each vector as own variable
+## your code here
+
+
+# pass data through LOW PASS FILTER (OPTIONAL)
+## your code here
+
+# pass data through HIGH PASS FILTER (OPTIONAL) to create BAND PASS result
+## your code here
+
+
+# pass data through differentiator
+## your code here
+
+# pass data through square function
+## your code here
+
+# pass through moving average window
+## your code here
+
+
+# use matplotlib to generate figures for each intermediate signal
+# you may wish to example the sampling rate for your selected file
+# to determine how many points for each vector to plot
+## your code here
---
+++ weekly-assignments/week5-python-beats.py
@@ -0,0 +1,18 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import the CSV file using numpy
+path = '../data/ekg/mitdb_201.csv'
+
+# load data in matrix from CSV file; skip first two rows
+
+## your code here
+
+# save each vector as own variable
+
+## your code here
+
+# use matplot lib to generate a single
+
+## your code here
