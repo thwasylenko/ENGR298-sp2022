556f688b12488195127dd7b0137d51ac871963c9
Jason Forsyth <forsy2jb@jmu.edu>
2022-03-01 17:44:41 -0500

    Compress class and files utils into same program

--- projects/covid-data/student_template.py
+++ projects/covid-data/student_template.py
@@ -32,32 +32,41 @@ class CovidRecord:
         self.date = _date
         self.county = _county
         self.state = _state
-        self.fips = _fips
-        self.cases = _cases
-        self.death = _date
+
+        if _fips =='':
+            self.fips=0
+        else:
+            self.fips = int(_fips)
+        self.cases = int(_cases)
+
+        if _death == '':
+            self.death = 0
+        else:
+            self.death = int(_death)
 
 
-# load covid data as list of CovidRecord objects
-data = load_with_pickle('covid_data.pickle')
+if __name__ == "__main__":
+    # load covid data as list of CovidRecord objects
+    data = load_with_pickle('covid_data.pickle')
 
-# each element in data is a CovidRecord object. Each of which contains
-# date, county, state, fips, cases, and deaths
+    # each element in data is a CovidRecord object. Each of which contains
+    # date, county, state, fips, cases, and deaths
 
-# for example, we can print out the data for the first point in the US counties file
-point = data[0]
+    # for example, we can print out the data for the first point in the US counties file
+    point = data[0]
 
-print("Data: ", point.date, " County: ", point.county, " State: ", point.state,
-      " FIPS: ", point.fips, " Cases: ", point.cases, " Deaths: ", point.death)
+    print("Data: ", point.date, " County: ", point.county, " State: ", point.state,
+          " FIPS: ", point.fips, " Cases: ", point.cases, " Deaths: ", point.death)
 
-# write code to address the following question:
-# When was the first positive COVID case in Rockingham County and Harrisonburg?
+    # write code to address the following question:
+    # When was the first positive COVID case in Rockingham County and Harrisonburg?
 
-# write code to address the following question:
-# What day was the greatest number of new daily cases recorded in Harrisonburg and Rockingham County?
+    # write code to address the following question:
+    # What day was the greatest number of new daily cases recorded in Harrisonburg and Rockingham County?
 
-# write code to address the following question:
-# In terms of absolute number of cases, when was the worst seven day period in the city/county for new COVID cases?
+    # write code to address the following question:
+    # In terms of absolute number of cases, when was the worst seven day period in the city/county for new COVID cases?
 
-# write code to address the following question:
-# In terms of absolute number of cases, when was the rise in cases the fastest over a rolling week window?
-# Over what period was the rise in cases the greatest
+    # write code to address the following question:
+    # In terms of absolute number of cases, when was the rise in cases the fastest over a rolling week window?
+    # Over what period was the rise in cases the greatest
