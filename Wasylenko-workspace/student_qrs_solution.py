6030a5e85aa90e4e327936898e568545b7f858bb
thwasylenko <thwasylenko@gmail.com>
2022-03-02 22:07:01 -0500

    commit
    commit

---
+++ Wasylenko-workspace/student_qrs_solution.py
@@ -0,0 +1,137 @@
+import numpy as np
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
+from ekg_testbench import EKGTestBench
+
+
+def main(filepath):
+    if filepath == '':
+        return list()
+
+    # import the CSV file using numpy
+    path = filepath
+
+    # load data in matrix from CSV file; skip first two rows
+    ## your code here
+    ekg_data = np.loadtxt(path, skiprows=2, delimiter=",")
+    # save each vector as own variable
+    ## your code here
+    time = ekg_data[:, 0]
+    v1 = ekg_data[:, 2]
+    # pass data through LOW PASS FILTER (OPTIONAL)
+    ## your code here
+
+    # pass data through HIGH PASS FILTER (OPTIONAL) to create BAND PASS result
+    ## your code here
+
+    # pass data through differentiator
+    ## your code here
+    diff = np.diff(v1)
+    # pass data through square function
+    ## your code here
+    fixed_diff = np.insert(diff, 0, 0)
+    square = np.square(fixed_diff)
+    # pass through moving average window
+    ## your code here
+    moving_avg = np.convolve(square, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    # take the output of the moving average and save it to 'signal' to it can be passed
+    # back to the testbench
+    signal = moving_avg
+
+    # use find_peaks to identify peaks within averaged/filtered data
+    # save the peaks result and return as part of testbench result
+    peaks,_ = find_peaks(signal, height=2, distance=100)
+
+    # do not modify this line
+    return signal, peaks
+
+
+# when running this file directly, this will execute first
+if __name__ == "__main__":
+
+    #database name
+    database_name='mitdb_213'
+
+    # set to true if you wish to generate a debug file
+    file_debug = True
+
+    # set to true if you wish to print overall stats to the screen
+    print_debug = True
+
+    # set to true if you wish to show a plot of each detection process
+    show_plot = True
+
+    ### DO NOT MODIFY BELOW THIS LINE!!! ###
+
+    # path to ekg folder
+    path_to_folder="../data/ekg/"
+
+    # select a signal file to run
+    signal_filepath = path_to_folder+database_name+".csv"
+
+    # call main() and run against the file. Should return the filtered
+    # signal and identified peaks
+    (signal,peaks)=main(signal_filepath)
+
+    # matched is a list of (peak, annotation) pairs; unmatched is a list of peaks that were
+    # not matched to any annotation; and remaining is annotations that were not matched.
+    annotation_path = path_to_folder+database_name+"_annotations.txt"
+    tb = EKGTestBench(annotation_path)
+    peaks_list = peaks.tolist()
+    (matched, unmatched, remaining) = tb.generate_stats(peaks_list)
+
+    # if was matched, then is true positive
+    true_positive = len(matched)
+
+    # if response was unmatched, then is false positive
+    false_positive = len(unmatched)
+
+    # whatever remains in annotations is a missed detection
+    false_negative = len(remaining)
+
+    # calculate f1 score
+    f1 = true_positive / (true_positive + 0.5 * (false_positive + false_negative))
+
+    # if we wish to show the resulting plot
+    if show_plot:
+        # make a nice plt of results
+        plt.title('Signal for ' + database_name + " with detections")
+
+        plt.plot(signal, label="Filtered Signal")
+        plt.plot(peaks, signal[peaks], 'p', label='Detected Peaks')
+
+        true_annotations = np.asarray(tb.annotation_indices)
+        plt.plot(true_annotations, signal[true_annotations], 'o', label='True Annotations')
+
+        plt.legend()
+
+        # uncomment line to show the plot
+        plt.show()
+
+    # if we wish to save all the stats to a file
+    if file_debug:
+        # print out more complex stats to the debug file
+        debug_file_path = database_name + "_debug_stats.txt"
+        debug_file = open(debug_file_path, 'w')
+
+        # print out indices of all false positives
+        debug_file.writelines("-----False Positives Indices-----\n")
+        for fp in unmatched:
+            debug_file.writelines(str(fp) + "\n")
+
+        # print out indices of all false negatives
+        debug_file.writelines("-----False Negatives Indices-----\n")
+        for fn in remaining:
+            debug_file.writelines(str(fn.sample) + "\n")
+
+        # close file that we writing
+        debug_file.close()
+
+    if print_debug:
+        print("-------------------------------------------------")
+        print("Database|\t\tTP|\t\tFP|\t\tFN|\t\tF1")
+        print(database_name, "|\t\t", true_positive, "|\t", false_positive, '|\t', false_negative, '|\t', round(f1, 3))
+        print("-------------------------------------------------")
+
+    print("Done!")
+
---
+++ Wasylenko-workspace/weekly assignments/week2-odds-evens.py
@@ -0,0 +1,45 @@
+# bring in randomness cause we need it in our lives
+import random
+
+
+# copy in Dr. Forsyth's random list function for use
+def generate_random_int_list(max_length, upper_bound):
+    # generate random length between 2 and max_length
+    list_length = int(random.uniform(2, max_length))
+
+    # given the length above, sample the Natural Numbers up to upper_bound that many times
+    vars = random.sample(range(upper_bound), list_length)
+
+    # return the generated list
+    return vars
+
+
+def main(random_list):
+    # variable to hold the odd or even count
+    # do not modify their names
+    num_even = 0
+    num_odd = 0
+
+    for num in random_list:
+        check = num % 2
+        if check == 1:
+            num_odd += 1
+        else:
+            num_even += 1
+
+    # do not modify this line
+    return (num_even, num_odd)
+
+
+if __name__ == "__main__":
+    # generate two random lists of integers
+    max_length = 20
+    upper_bound = 100
+    nums = generate_random_int_list(max_length, upper_bound)
+
+    # pass the random list to the student main
+    (odd, even) = main(nums)
+
+    # print out the list and see the results
+    print("You were sent the list: ", nums)
+    print("Your solution reported ", odd, " odd values and ", even, "even values.")
