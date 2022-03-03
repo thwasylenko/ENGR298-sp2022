186345c96b380529eda05a7dfb4f8966af0ca8d3
Jason Forsyth <forsy2jb@jmu.edu>
2022-03-03 00:23:53 -0500

    Add odds-evens template

--- weekly-assignments/week3-odds-evens.py
+++ weekly-assignments/week3-odds-evens.py
@@ -1,3 +1,4 @@
+# bring in randomness cause we need it in our lives
 import random
 
 
@@ -7,22 +8,40 @@ def generate_random_int_list(max_length, upper_bound):
     list_length = int(random.uniform(2, max_length))
 
     # given the length above, sample the Natural Numbers up to upper_bound that many times
-    vars = random.sample(range(upper_bound), list_length)
+    random_vars = random.sample(range(upper_bound), list_length)
 
     # return the generated list
-    return vars
+    return random_vars
 
 
-# generate two random lists of integers
-max_length = 20
-upper_bound = 100
-nums = generate_random_int_list(max_length, upper_bound)
+def main(random_list):
+    """
+    Given a random list of integers, return two lists of only even and odd values from that random list
+    :param random_list: A random list
+    :return: A tuple containing (even , odd) list
+    """
+    # lists to hold the even and odd numbers
+    # do not modify their names
+    evens_list = []
+    odds_list = []
 
-# scan the list nums and place odds and evens in different lists
-odds = list()
-evens = list()
+    ### Your code here ###
 
-### YOUR CODE HERE ###
 
-print("Odds: ", odds)
-print("Evens: ", evens)
+    # do not modify this line
+    return (evens_list, odds_list)
+
+
+if __name__ == "__main__":
+    # generate two random lists of integers
+    max_length = 20
+    upper_bound = 100
+    nums = generate_random_int_list(max_length, upper_bound)
+
+    # pass the random list to the student main
+    (even, odd) = main(nums)
+
+    # print out the list and see the results
+    print("You were passed the list: ", nums)
+    print("Here are all the evens: ", even)
+    print("Here are all the odds: ", odd)
